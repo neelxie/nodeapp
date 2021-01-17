c0bdbdc6efcfe8e572385df89c0795cab4053752
neelxie <kidricederek@gmail.com>
2021-01-18 02:21:23 +0300

    create members

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Snow',
+        email: 'johnsnow@cia.gov',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'John Doe',
+        email: 'johndoe@cia.gov',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Jane Snow',
+        email: 'janesnow@cia.gov',
+        status: 'inactive'
+    },
+    {
+        id: 4,
+        name: 'Jane Doe',
+        email: 'janedoe@cia.gov',
+        status: 'active'
+    },
+];
+
+modeule.exports = members;
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require("express");
 const path = require('path');
+const members = require('./Members');
 
 const app = express();
+
+
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
+
+app.get('api/members', (req, res) => {
+    res.json(members);
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
