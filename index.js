3b99e392a6d056e05a4c2229cfa329cd544c19df
neelxie <kidricederek@gmail.com>
2021-01-18 01:47:49 +0300

    add index file

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send('Greatest!');
+});
+
+var port = Number(process.env.PORT || 5000);
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+// npm i -D nodemon
+// dev env to prevent reloaading our server
