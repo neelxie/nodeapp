b2e105d2a42160dace8fe435bde98e8718df8b28
neelxie <kidricederek@gmail.com>
2021-01-25 04:57:34 +0300

    create middleware

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 var express = require("express");
-const path = require('path');
-const moment =  require('moment');
-const members = require('./Members');
+const path = require("path");
+const moment = require("moment");
+const logger = require('./middleware/logger');
+const members = require("./Members");
 
 const app = express();
-
-const logger = (req, res, next) => {
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
-    next();
-};
 
 app.use(logger);
 
@@ -16,13 +12,12 @@ app.use(logger);
 //   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
 
-app.get('/api/members', (req, res) => 
-    res.json(members));
+app.get("/api/members", (req, res) => res.json(members));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 var port = process.env.PORT || 5000;
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Listening on Port:" + port);
 });
